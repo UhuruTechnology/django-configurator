720a656f206962e2d7cf223b50566a348dde2ce2
Christopher Broderick <uhurusurfa@gmail.com>
2025-08-15 17:12:04 +0100

    Fix tests relying on global ALLOWED_HOSTS being available.

--- tests/settings/mixin_inheritance.py
+++ tests/settings/mixin_inheritance.py
@@ -4,7 +4,10 @@ from dj_configurator import Configuration
 class Mixin1:
     @property
     def ALLOWED_HOSTS(self):
-        allowed_hosts = super().ALLOWED_HOSTS[:]
+        try:
+            allowed_hosts = super().ALLOWED_HOSTS[:]
+        except Exception:
+            allowed_hosts = []
         allowed_hosts.append("test1")
         return allowed_hosts
 
@@ -12,13 +15,19 @@ class Mixin1:
 class Mixin2:
     @property
     def ALLOWED_HOSTS(self):
-        allowed_hosts = super().ALLOWED_HOSTS[:]
+        try:
+            allowed_hosts = super().ALLOWED_HOSTS[:]
+        except Exception:
+            allowed_hosts = []
         allowed_hosts.append("test2")
         return allowed_hosts
 
 
 class Inheritance(Mixin2, Mixin1, Configuration):
     def ALLOWED_HOSTS(self):
-        allowed_hosts = super().ALLOWED_HOSTS[:]
+        try:
+            allowed_hosts = super().ALLOWED_HOSTS[:]
+        except Exception:
+            allowed_hosts = []
         allowed_hosts.append("test3")
         return allowed_hosts
--- tests/settings/single_inheritance.py
+++ tests/settings/single_inheritance.py
@@ -4,6 +4,9 @@ from .base import Base
 class Inheritance(Base):
     @property
     def ALLOWED_HOSTS(self):
-        allowed_hosts = super().ALLOWED_HOSTS[:]
+        try:
+            allowed_hosts = super().ALLOWED_HOSTS[:]
+        except Exception:
+            allowed_hosts = []
         allowed_hosts.append("test")
         return allowed_hosts
